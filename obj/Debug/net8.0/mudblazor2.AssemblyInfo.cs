62463c24fb78098a6c9308624d7f2a8b0b525b6d
AndrewTJW <130200621+AndrewTJW@users.noreply.github.com>
2024-11-08 00:35:22 +0800

    as of 12:35am 11/8/2024

--- obj/Debug/net8.0/mudblazor2.AssemblyInfo.cs
+++ obj/Debug/net8.0/mudblazor2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mudblazor2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe1f8e27666f099a74d7b00b4a08633e72a53cbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("mudblazor2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mudblazor2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
